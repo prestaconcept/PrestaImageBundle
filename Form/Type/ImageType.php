16bb66be1e998ebee9a3e96ca33d8e2814f41a02
Benoit Jouhaud <bjouhaud@prestaconcept.net>
2016-06-01 14:36:02 +0200

    #2 - rework ImageType to extend VichFileType the Symfony way

--- Form/Type/ImageType.php
+++ Form/Type/ImageType.php
@@ -3,32 +3,46 @@
 namespace Presta\ImageBundle\Form\Type;
 
 use Presta\ImageBundle\Model\Cropper\AspectRatio;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 /**
  * @author Benoit Jouhaud <bjouhaud@prestaconcept.net>
  */
-class ImageType extends VichFileType
+class ImageType extends AbstractType
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('base64', HiddenType::class, [
-            'attr' => [
-                'class' => 'cropper-base64',
-            ],
-        ]);
-
-        if ($options['allow_delete']) {
-            $this->buildDeleteField($builder, $options);
-        }
+        $builder
+            ->add('base64', HiddenType::class, [
+                'attr' => [
+                    'class' => 'cropper-base64',
+                ],
+            ])
+            ->remove('file')
+        ;
     }
 
     /**
@@ -36,8 +50,6 @@ class ImageType extends VichFileType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        parent::configureOptions($resolver);
-
         $resolver
             ->setDefault('aspect_ratios', [
                 '16_9' => new AspectRatio(1.78, $this->translator->trans('aspect_ratio.16_9')),
@@ -56,10 +68,16 @@ class ImageType extends VichFileType
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        parent::buildView($view, $form, $options);
-
         $view->vars['aspect_ratios'] = $options['aspect_ratios'];
         $view->vars['max_width'] = $options['max_width'];
         $view->vars['max_height'] = $options['max_height'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return VichFileType::class;
     }
 }
