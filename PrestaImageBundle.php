17a95f7a63f40cc76ba4c470713275fa30059526
Benoit Jouhaud <bjouhaud@prestaconcept.net>
2016-06-01 14:41:16 +0200

    #1 - add bundle class

---
+++ PrestaImageBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Presta\ImageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Benoit Jouhaud <bjouhaud@prestaconcept.net>
+ */
+class PrestaImageBundle extends Bundle
+{
+}
