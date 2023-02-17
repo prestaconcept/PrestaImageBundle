b55abaafefd94f7ab0940de8bc4faab3f9797a29
Benoit Jouhaud <bjouhaud@prestaconcept.net>
2023-02-17 16:21:23 +0100

    Remove unused import

--- src/Controller/UrlToBase64Controller.php
+++ src/Controller/UrlToBase64Controller.php
@@ -3,7 +3,6 @@
 namespace Presta\ImageBundle\Controller;
 
 use Presta\ImageBundle\Helper\Base64Helper;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
