e096b2eb54b8fb267670fd75be8aaf866f71b7e8
Yann Eugoné <yeugone@prestaconcept.net>
2026-04-10 10:56:07 +0200

    Changed deprecated extension base class

--- src/DependencyInjection/PrestaImageExtension.php
+++ src/DependencyInjection/PrestaImageExtension.php
@@ -6,8 +6,8 @@ namespace Presta\ImageBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class PrestaImageExtension extends Extension
 {
